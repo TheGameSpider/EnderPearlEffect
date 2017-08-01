1ed5b17cd0936a15ccbbaf647f3fce6258fdb5b1
TheGameSpider <thegamespider20@gmail.com>
2017-08-01 23:31:01 +0200

    Fxed :)

--- src/TheGameSpider/plugins/EnderPearlEffect/effect.java
+++ src/TheGameSpider/plugins/EnderPearlEffect/effect.java
@@ -5,16 +5,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
-import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 
 public class effect implements Listener {
 
 	@EventHandler
 	public void onTP(PlayerTeleportEvent event) {
 		Player p = event.getPlayer();
-		if(event.getCause() == TeleportCause.ENDER_PEARL) {
-			p.getWorld().playEffect(p.getLocation(), Effect.ENDER_SIGNAL, 1);
-		}
+			p.getWorld().playEffect(event.getTo(), Effect.ENDER_SIGNAL, 10);
+		
 		
 	}
 }
